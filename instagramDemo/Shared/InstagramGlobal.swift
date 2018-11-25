6c71f9e89d4f37fd3c1f1548755fe24e0b6088d1
Fatma Dağdevir <fatma.dagdevir@ingbank.com.tr>
2018-11-25 21:40:24 +0300

    Instagram API Authorization

---
+++ instagramDemo/Shared/InstagramGlobal.swift
@@ -0,0 +1,9 @@
+//
+//  InstagramGlobal.swift
+//  instagramDemo
+//
+//  Created by Fatma Dagdevir [Set Yazilim] on 11/25/18.
+//  Copyright © 2018 Fatma Dagdevir. All rights reserved.
+//
+
+import Foundation
